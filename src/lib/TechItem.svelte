7476c4fda1f6a71681039174c84a6da4aa0d831b
Jip Fr <jipfrijlink@gmail.com>
2021-11-07 22:34:59 +0100

    feat(pages): implement "technologies" on the home page

---
+++ src/lib/TechItem.svelte
@@ -0,0 +1,53 @@
+<script>
+	export let title;
+	export let subtitle;
+	export let iconSource;
+	export let bg;
+</script>
+
+<div class="tech-item" style={`background-color: ${bg};`}>
+	<div>
+		<img src={iconSource} alt />
+	</div>
+	<div class="tech-item-core">
+		<p class="title">{title}</p>
+		<p class="subtitle">{subtitle}</p>
+	</div>
+</div>
+
+<style lang="scss">
+	.tech-item {
+		padding: 10px;
+		display: grid;
+		grid-template-columns: 35px 1fr;
+		grid-gap: 20px;
+		align-items: center;
+		border-radius: 6px;
+
+		img {
+			display: block;
+			width: 100%;
+			height: 35px;
+			object-fit: contain;
+		}
+
+		p {
+			margin: 0;
+			color: var(--text-title);
+		}
+
+		.title {
+			font-weight: bold;
+		}
+
+		.subtitle {
+			opacity: 0.6;
+		}
+	}
+
+	@media (prefers-color-scheme: dark) {
+		.tech-item {
+			background-color: var(--header-bg) !important; // Override style attribute
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <script>
-	import Container from '$lib/layout/Container.svelte'
+	import Container from '$lib/layout/Container.svelte';
+	import TechItem from '$lib/TechItem.svelte';
 </script>
 
 <svelte:head>
@@ -12,7 +13,58 @@
 
 <Container>
 	<h1>About me</h1>
+
 	<p class="paragraph">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Hello there! I am a full-stack web developer from the Netherlands. I know HTML, (S)CSS,
+		JavaScript and TypeScript, Node.js, and Vue.
 	</p>
+
+	<h2>Find me online</h2>
+
+	<p class="paragraph">
+		I have a GitHub account where I share many of my projects at <a
+			href="https://github.com/jipfr"
+			target="_blank"
+			class="anchor">@jipfr</a
+		>. You can e-mail me at
+		<a class="anchor" href="mailto:contact@jipfr.nl" target="_blank">contact@jipfr.nl</a>.
+	</p>
+
+	<h2>I have experience with</h2>
+
+	<p class="paragraph">To categorise my knowledge, I have the following levels</p>
+
+	<ol class="paragraph">
+		<li>I've used this technology in the past and I'm able to figure it out from there</li>
+		<li>I have written an app or website using this technology</li>
+		<li>I'm confident with this technology</li>
+	</ol>
+
+	<div class="tech-list">
+		<TechItem
+			title="TypeScript / JS"
+			subtitle="Level 3"
+			iconSource="/img/tech/typescript.svg"
+			bg="#deeeff"
+		/>
+		<TechItem title="Nuxt / Vue" subtitle="Level 3" iconSource="/img/tech/nuxt.png" bg="#EBF6F3" />
+		<TechItem title="Svelte" subtitle="Level 2" iconSource="/img/tech/svelte.svg" bg="#ffe8e0" />
+		<TechItem title="React" subtitle="Level 2" iconSource="/img/tech/react.svg" bg="#e8faff" />
+		<TechItem
+			title="Tailwind"
+			subtitle="Level 2"
+			iconSource="/img/tech/tailwind.svg"
+			bg="#defaff"
+		/>
+		<TechItem title="HTML" subtitle="Level 3" iconSource="/img/tech/html.png" bg="#F3E9E6" />
+		<TechItem title="(S)CSS" subtitle="Level 3" iconSource="/img/tech/scss.png" bg="#fcd7ea" />
+	</div>
 </Container>
+
+<style lang="scss">
+	.tech-list {
+		display: grid;
+		grid-gap: 10px;
+		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+	}
+</style>
