c586b23eebabd68dd79c4ab9c49cb2d5ce136c74
Jip Fr <jipfrijlink@gmail.com>
2021-12-11 22:37:53 +0100

    feat(contributions): add images grid with zoom

---
+++ src/lib/util/Images.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Zooming from 'zooming';
+	import { onMount } from 'svelte';
+
+	export let images = {};
+
+	onMount(() => {
+		const zooming = new Zooming({
+			onBeforeOpen: (el) => {
+				console.log(el);
+				el.style.height = 'auto';
+			},
+			onBeforeClose: (el) => {
+				el.style.height = '200px';
+			},
+			onClose: (el) => {
+				el.style.height = '200px';
+			},
+			bgColor: 'var(--body)'
+		});
+
+		zooming.listen('.img-zoomable');
+	});
+</script>
+
+<div class="images">
+	{#each Object.entries(images) as [i, image]}
+		<img src={image.src} alt={image.alt || ''} class="img-zoomable" />
+	{/each}
+</div>
+
+<style lang="scss">
+	.images {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+		grid-gap: 10px;
+
+		:global(img) {
+			margin: 0;
+			width: 100%;
+			height: 200px;
+			object-fit: cover;
+			border-radius: 4px;
+		}
+	}
+</style>
