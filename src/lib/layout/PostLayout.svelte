d14089fae0ab838efa3da69294f2be5831c7a885
Jip Fr <jipfrijlink@gmail.com>
2021-12-14 17:00:53 +0100

    style(blog): add color highlighting, add date

--- src/lib/layout/PostLayout.svelte
+++ src/lib/layout/PostLayout.svelte
@@ -7,12 +7,15 @@
 	export let description;
 	export let left;
 	export let image;
+	export let date;
 
 	import Title from '$lib/util/Title.svelte';
+	import Image from '$lib/util/Image.svelte';
+	import Description from '$lib/util/Description.svelte';
 
 	import Container from '$lib/layout/Container.svelte';
-	import Description from '$lib/util/Description.svelte';
-	import Image from '$lib/util/Image.svelte';
+
+	import { getDate } from '../../helpers';
 </script>
 
 <svelte:head>
@@ -24,7 +27,11 @@
 <Container>
 	<div class="post is-md-post">
 		<h1 class:left>{title}</h1>
+		{#if date}
+			<h2 class="date">{getDate(date)}</h2>
+		{/if}
 		<slot />
+		<link rel="stylesheet" href="/one-dark.css" />
 	</div>
 </Container>
 
@@ -34,15 +41,30 @@
 		text-align: center;
 	}
 
+	h2.date {
+		font-size: 1.1rem;
+		width: 100%;
+		text-align: center;
+		color: var(--text);
+		margin: 20px 0;
+		margin-bottom: 40px;
+		font-weight: normal;
+	}
+
 	.post.is-md-post :global(iframe) {
 		width: 100%;
 		height: 500px;
 		border: 1px solid var(--border);
 	}
 
-	.post.is-md-post :global(code) {
-		background: var(--content);
-		padding: 2px;
-		border-radius: 4px;
+	.post.is-md-post :global(pre code) {
+		font-size: 0.8rem;
+	}
+
+	@media (max-width: 700px) {
+		h1:not(.left) {
+			text-align: left;
+			font-size: 1.2rem;
+		}
 	}
 </style>
