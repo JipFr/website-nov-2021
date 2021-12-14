044200cee4008e7a0e4ddf508a25cc7bc4e9b50d
Jip Fr <jipfrijlink@gmail.com>
2021-12-14 18:23:21 +0100

    feat(util): add "questions or concerns" component

---
+++ src/lib/util/QuestionsAndConcerns.svelte
@@ -0,0 +1,13 @@
+<div class="contact-box">
+	<p>Any questions or concerns? You can send me an e-mail!</p>
+	<p><a href="mailto:jip@frij.link">jip@frij.link</a></p>
+</div>
+
+<style lang="scss">
+	.contact-box {
+		width: 100%;
+		padding: 40px;
+		text-align: center;
+		border: 1px solid var(--border);
+	}
+</style>
