70ff6e94726a7e1a6eebe2afad9b94b376d77c9c
Jip Fr <jipfrijlink@gmail.com>
2021-12-15 22:47:35 +0100

    feat(blog): build basic code viewer

---
+++ src/lib/util/Code.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Prism from 'prismjs';
+
+	export let code = '';
+	export let lang;
+
+	$: highlighted = Prism.highlight(code, Prism.languages[lang], lang);
+	console.log(code, highlighted);
+</script>
+
+<pre class={`language-${lang}`}>
+	<code class={`language-${lang}`}>
+		{@html highlighted}
+	</code>
+</pre>
+
+<link rel="stylesheet" href="/one-dark.css" />
+
+<style>
+	pre {
+		max-width: 100%;
+		max-height: 500px;
+		overflow: scroll;
+		font-size: 0.8rem;
+	}
+</style>
---
+++ src/lib/util/CodeViewer.svelte
@@ -0,0 +1,210 @@
+<script>
+	import Code from '$lib/util/Code.svelte';
+
+	export let html = '';
+	export let css = '';
+	export let js = '';
+
+	let iframeWrapper = null;
+	let codeViewer = null;
+	let codeWidth = 0;
+	let fromRight = 0;
+	let mouseIsDown = false;
+	let lang;
+	let selectedCode;
+
+	if (html) {
+		lang = 'html';
+		selectedCode = html;
+	} else if (css) {
+		lang = 'css';
+		selectedCode = css;
+	} else if (js) {
+		lang = 'javascript';
+		selectedCode = js;
+	}
+
+	$: iframeWrapper, updateIframe();
+
+	function onMouseDown() {
+		mouseIsDown = true;
+	}
+	function onMouseUp() {
+		mouseIsDown = false;
+	}
+
+	function updateIframe() {
+		if (typeof document !== 'undefined' && iframeWrapper) {
+			const iframe = document.createElement('iframe');
+			iframe.src = '/blank.html';
+			iframe.setAttribute('frameborder', 0);
+
+			iframe.onload = () => {
+				if (html) {
+					iframe.contentDocument.body.innerHTML = html;
+				}
+
+				if (css) {
+					const style = document.createElement('style');
+					style.innerHTML = css;
+					iframe.contentDocument.body.appendChild(style);
+				}
+
+				if (js) {
+					const script = document.createElement('script');
+					script.innerHTML = js;
+					iframe.contentDocument.body.appendChild(script);
+				}
+			};
+
+			iframeWrapper.innerHTML = '';
+			iframeWrapper.appendChild(iframe);
+		}
+	}
+
+	function onMouseMove(evt) {
+		let containerWidth = codeViewer.scrollWidth - 30;
+		fromRight = containerWidth - codeWidth;
+
+		if (mouseIsDown) {
+			if (
+				(evt.movementX > 0 && evt.clientX > codeViewer.querySelector('.mover').offsetLeft + 20) ||
+				(evt.movementX < 0 && evt.clientX < codeViewer.querySelector('.mover').offsetLeft + 20)
+			) {
+				codeWidth = Math.min(Math.max(codeWidth + evt.movementX, 0), containerWidth);
+			}
+		}
+	}
+
+	function setTab(newLang) {
+		lang = newLang;
+		if (lang === 'html') {
+			selectedCode = html;
+		} else if (lang === 'css') {
+			selectedCode = css;
+		} else if (lang === 'js') {
+			selectedCode = js;
+		}
+		console.log(selectedCode);
+	}
+</script>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
+
+<div class="code-viewer" bind:this={codeViewer}>
+	<div class="code-side" style={`--width: ${codeWidth}px; ${codeWidth < 250 && 'display: none;'}`}>
+		<div class="tabs">
+			{#if html}
+				<div class="tab html" on:click={() => setTab('html')} class:active={lang === 'html'}>
+					<img src="/img/tech/html.png" alt="HTML" />
+					<span>HTML</span>
+				</div>
+			{/if}
+			{#if css}
+				<div class="tab css" on:click={() => setTab('css')} class:active={lang === 'css'}>
+					<img src="/img/tech/css.svg" alt="CSS" />
+					<span>CSS</span>
+				</div>
+			{/if}
+			{#if js}
+				<div class="tab js" on:click={() => setTab('js')} class:active={lang === 'js'}>
+					<img src="/img/tech/js.png" alt="JavaScript" />
+					<span>JS</span>
+				</div>
+			{/if}
+		</div>
+		<Code code={selectedCode} {lang} />
+	</div>
+	<div class="mover" on:mousedown={onMouseDown} on:mouseup={onMouseUp} />
+	<div
+		class="viewing-side"
+		bind:this={iframeWrapper}
+		style={`${fromRight < 100 && 'opacity: 0; max-height: 0; overflow: hidden;'}; ${
+			mouseIsDown && 'pointer-events: none;'
+		}`}
+	/>
+</div>
+
+<style lang="scss">
+	.code-viewer {
+		display: flex;
+		width: 100%;
+		min-height: 500px;
+		margin: 30px 0;
+		border: 1px solid var(--border);
+		background: #282c34;
+
+		.code-side {
+			width: var(--width);
+			min-width: var(--width);
+			max-width: 300px;
+		}
+
+		.mover {
+			min-width: 30px;
+			width: 30px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			cursor: col-resize;
+
+			&::before {
+				content: '';
+				display: block;
+				width: 3px;
+				height: 80%;
+				background: rgba(255, 255, 255, 0.1);
+			}
+		}
+
+		.viewing-side {
+			width: 100%;
+		}
+	}
+	.viewing-side :global(iframe) {
+		width: 100%;
+		height: 100%;
+	}
+
+	.code-side {
+		.tabs {
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+			grid-gap: 10px;
+			padding: 10px;
+
+			.tab {
+				padding: 5px 10px;
+				width: 100%;
+				cursor: pointer;
+				color: white;
+				background: #282c34;
+				text-align: left;
+				display: flex;
+				flex-wrap: wrap;
+				align-items: center;
+
+				img {
+					display: block;
+					width: 1.2rem;
+					height: 1.2rem;
+					margin-right: 10px;
+				}
+
+				span {
+					display: block;
+					margin-top: 5px;
+					min-width: 50px;
+				}
+
+				&:hover {
+					background: #1d2026;
+				}
+
+				&.active {
+					background: #121417;
+				}
+			}
+		}
+	}
+</style>
