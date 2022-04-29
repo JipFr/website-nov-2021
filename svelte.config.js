1f9eb4e8904c37696fde21825c76ca7d8ec91f66
Jip Frijlink <jipfrijlink@gmail.com>
2022-04-29 21:52:18 +0000

    fix: update SvelteKit stuff to work

--- svelte.config.js
+++ svelte.config.js
@@ -15,8 +15,6 @@ const config = {
 			assets: 'build',
 			fallback: null
 		}),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
 		vite: {
 			plugins: [
 				mdPlugin.default({
